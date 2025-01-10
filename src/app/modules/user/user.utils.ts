79883e07080c17d13d1111d348dca29d1df00b33
riyaz-369 <riyazulislam0099@gmail.com>
2025-01-10 23:13:22 +0600

    fix bug for id generate

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,11 +16,26 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester | null) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId(); //2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); // 01
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4); // 2030
+  const currentSemesterCode = payload?.code;
+  const currentSemesterYear = payload?.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentSemesterYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId.substring(6); // 0002
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
   incrementId = `${payload?.year}${payload?.code}${incrementId}`;
   return incrementId;
